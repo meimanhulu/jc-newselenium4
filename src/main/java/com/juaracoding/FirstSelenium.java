9c56c068c20ec1af0dc0968eebd6bc8a9dedfb85
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-01-22 13:08:55 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/FirstSelenium.java
@@ -0,0 +1,51 @@
+package com.juaracoding;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstSelenium {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.out.println("Selenium 4");
+        System.setProperty("webdriver.chrome.driver","C:\\MyTools\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+
+        // locator
+        login("standard_user","secret_sauce");
+        // step logout id=react-burger-menu-btn, logout_sidebar_link
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        driver.findElement(By.id("logout_sidebar_link")).click();
+
+        login("locked_out_user","secret_sauce");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
+
+    }
+
+    // method login
+    private static void login(String username, String password){
+        driver.findElement(By.name("user-name")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("login-button")).click();
+    }
+
+    // custom assertion if
+
+}
