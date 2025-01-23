978fc7a94d21e795fe33536df9551d07c4759b45
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-01-23 20:44:42 +0700

    push

--- src/main/java/com/juaracoding/FirstSelenium.java
+++ src/main/java/com/juaracoding/FirstSelenium.java
@@ -1,10 +1,13 @@
 package com.juaracoding;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class FirstSelenium {
 
@@ -17,35 +20,72 @@ public class FirstSelenium {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
-
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
 
+        String username = "standard_user";
+
         // locator
-        login("standard_user", "secret_sauce");
-        // step logout id=react-burger-menu-btn, logout_sidebar_link
-        driver.findElement(By.id("react-burger-menu-btn")).click();
-        driver.findElement(By.id("logout_sidebar_link")).click();
+        login(username,"secret_sauce");
 
-        login("locked_out_user", "secret_sauce");
+        // step validation
+        WebElement titleWeb = driver.findElement(By.xpath("//span[@class='title']"));
+        String actual = titleWeb.getText();
+        String expected = "Products";
+        assertEquals(actual,expected);
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        // Collection
+        List<WebElement> listProduct = driver.findElements(By.xpath("//div[@class='inventory_item_name ']"));
+        System.out.println("Jumlah product = "+listProduct.size());
+        for (int i = 0; i < listProduct.size(); i++) {
+            System.out.println(listProduct.get(i).getText());
         }
+
+        logout();
+        WebElement elmUsername = driver.findElement(By.xpath("//input[@id='user-name']"));
+        elmUsername.sendKeys("standard_user");
+        elmUsername.sendKeys(Keys.CONTROL+"A");
+
+//        login("locked_out_user","secret_sauce");
+
+        System.out.println(username.contains("user"));
+
+        // thread sleep
+        delay(1000);
 
         driver.quit();
 
     }
 
     // method login
-    private static void login(String username, String password) {
-        driver.findElement(By.name("user-name")).sendKeys(username);
-        driver.findElement(By.name("password")).sendKeys(password);
-        driver.findElement(By.name("login-button")).click();
+    private static void login(String username, String password){
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
     }
 
-    // custom assertion if
+    private static void logout(){
+        // step logout id=react-burger-menu-btn, logout_sidebar_link
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        driver.findElement(By.linkText("Logout")).click();
+    }
+
+    public static void delay(long second){
+        try {
+            Thread.sleep(second*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // custom assertion if assertEquals()
+    public static void assertEquals(String actual, String expected){
+        if(actual.equals(expected)){
+            System.out.println("passed");
+        } else {
+            System.out.println("failed");
+        }
+    }
 
 }
